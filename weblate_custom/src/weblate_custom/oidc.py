39fcbf98c738e6466d727ff3df5359070df7d936
Funasitien 🌥 <bwha.bwha.splash@gmail.com>
2026-08-11 11:08:52 +0200

    Refactor OIDC auth to use OpenIdConnectAuth
    Updated OIDC authentication backend to use OpenIdConnectAuth and removed unnecessary methods and properties.

--- weblate_custom/src/weblate_custom/oidc.py
+++ weblate_custom/src/weblate_custom/oidc.py
@@ -1,26 +1,9 @@
-from social_core.backends.oidc import BaseOIDCAuth
+from social_core.backends.open_id_connect import OpenIdConnectAuth
 
-class CustomOidcPkceAuth(BaseOIDCAuth):
+class CustomOidcPkceAuth(OpenIdConnectAuth):
     """Custom OIDC authentication backend with mandatory PKCE support."""
     
     name = 'custom-oidc-pkce'
-    EXTRA_DATA = [
-        ('refresh_token', 'refresh_token'),
-        ('expires_in', 'expires_in'),
-        ('id_token', 'id_token'),
-    ]
-
-    def get_settings(self, name):
-        """Override settings lookup to use custom prefix namespaces."""
-        return super().get_settings(name)
-
-    def auth_extra_arguments(self):
-        """Inject any required custom parameters into the authorization request."""
-        params = super().auth_extra_arguments()
-        # You can add custom audience or prompt options here if needed, e.g.:
-        # params['prompt'] = 'select_account'
-        return params
-
-    def compute_code_challenge(self):
-        """Indicate that PKCE should be used by providing code challenge settings."""
-        return True
+    
+    # social_core handles standard OIDC discovery, token exchange, 
+    # and automatic PKCE/S256 code verifier generation out-of-the-box.
